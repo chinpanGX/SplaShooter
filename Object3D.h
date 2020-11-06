8f2b594d0eaceb0b85651fc8788e8c2bc1134fb0
ChinpanGX <xchuhe@gameil.com>
2020-11-06 21:19:47 +0900

    3Dオブジェクトを作りたいときに使えるクラスを作った。

---
+++ Object3D.h
@@ -0,0 +1,18 @@
+/*-----------------------------------------------------------
+
+	[Object3D.h]
+
+	Author : èoçá„ƒëæ
+
+------------------------------------------------------------*/
+#pragma once
+#include "Shader.h"
+#include "Renderer.h"
+
+class Object3D
+{
+private:
+	ID3D11Buffer* m_VertexBuffer = NULL;
+
+};
+
