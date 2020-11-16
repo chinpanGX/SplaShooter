887d26f27da7ebaa71f3102029e71be1022a2e5c
ChinpanGX <xchuhe@gameil.com>
2020-11-16 22:06:03 +0900

    TestMap2の追加

--- Polygon3D.h
+++ Polygon3D.h
@@ -15,7 +15,7 @@ public:
 	Polygon3D() = delete; // 空のコンストラクタを呼ばないように
 	Polygon3D(Wrapper::DirectX11& dx, Wrapper::VERTEX_3D Vertex[4]);
 	~Polygon3D();
-	void Set(Wrapper::DirectX11& instance, D3DXVECTOR3 Position, D3DXVECTOR3 Rotation, D3DXVECTOR3 Scale);
-	void DrawPolygon(Wrapper::DirectX11 & instance);
+	void Draw(Wrapper::DirectX11& instance, D3DXVECTOR3 Position, D3DXVECTOR3 Rotation, D3DXVECTOR3 Scale);
+	//void DrawPolygon(Wrapper::DirectX11 & instance);
 };
 
--- TestScene.cpp
+++ TestScene.cpp
@@ -5,7 +5,9 @@
 #include "Camera.h"
 #include "Effect.h"
 #include "TestField.h"
+#include "Test.h"
 
+// 画面遷移
 void ChangeScene::ReturnTitle()
 {
 	if (KeyBoard::IsTrigger(DIK_R))
@@ -30,6 +32,7 @@ void ChangeScene::TestMap2()
 	}
 }
 
+// テストマップ１
 void Test::TestMap1::Init()
 {
 	AddGameObject<Camera>(LAYER_CAMERA);
@@ -52,18 +55,24 @@ void Test::TestMap1::Draw()
 	Scene::Draw();
 }
 
+// テストマップ２
 void Test::TestMap2::Init()
 {
+	AddGameObject<Camera>(LAYER_CAMERA);
+	AddGameObject<Test_Chinapan::Field>(LAYER_3D);
 }
 
 void Test::TestMap2::Uninit()
 {
+	TestMap1::Uninit();
 }
 
 void Test::TestMap2::Update()
 {
+	TestMap1::Update();
 }
 
 void Test::TestMap2::Draw()
 {
+	TestMap1::Draw();
 }
